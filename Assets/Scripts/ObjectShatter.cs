bfe165f9b709e2fd4cdb51595782a4c9c68ca72d
Kuroiden <118356075+Kuroiden@users.noreply.github.com>
2026-07-12 19:40:30 +0800

    Updated Physics Engine

--- Assets/Scripts/ObjectShatter.cs
+++ Assets/Scripts/ObjectShatter.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Net;
 using Unity.XR.CoreUtils;
 using UnityEngine;
 
@@ -8,7 +9,7 @@ public class ObjectShatter : MonoBehaviour
     Vector3 ObjectVelocity;
 
     public bool isReleased = false;
-    [SerializeField] float BreakagePoint; [Space]
+    [SerializeField][Range(0f, 3f)] float BreakagePoint; [Space]
 
     [SerializeField] List<GameObject> ObjectPieces;
 
@@ -20,13 +21,14 @@ public class ObjectShatter : MonoBehaviour
     void Awake()
     {
         MainRB = GetComponent<Rigidbody>();
+        MainRB.mass = Mass;
 
         for (int i = 0; i < transform.childCount; i++)
         {
             ObjectPieces.Add(transform.GetChild(i).gameObject);
         }
 
-        Debug.Log($"Pieces found in {this}: {ObjectPieces.Count}");
+        Debug.Log($"Pieces found in {this.gameObject.name}: {ObjectPieces.Count}");
     }
 
     void Update()
@@ -42,11 +44,44 @@ public class ObjectShatter : MonoBehaviour
     {
         if (ObjectPieces[0].GetComponent<Rigidbody>() != null) return;
 
-        Debug.Log($"{this} collided with {collision} (Velocity: {ObjectVelocity})");
+        Vector3 CollisionVelocity = ObjectVelocity;
 
-        if (ObjectVelocity == Vector3.zero) return;
+        if (ObjectVelocity == Vector3.zero)
+        {
+            GameObject ColliderObj = collision.collider.gameObject;
+
+            if (ColliderObj.GetComponent<Rigidbody>() != null)
+            {
+                Rigidbody ColliderRB = ColliderObj.GetComponent<Rigidbody>();
+                float ColliderMass = ColliderRB.mass * 100f;
+
+                // Checks for any collider that hits the breakble directly while at rest
+                bool ColliderMovement = true;
+
+                // Collider mass is also considered
+                Vector3 ColliderLinearVelocity = ColliderRB.linearVelocity * ColliderMass;
+                Vector3 ColliderAngularVelocity = ColliderRB.angularVelocity * ColliderMass;
+
+                CollisionVelocity = ColliderLinearVelocity != Vector3.zero ? ColliderLinearVelocity : ColliderAngularVelocity != Vector3.zero ? ColliderAngularVelocity : Vector3.zero;
+
+                if (ColliderLinearVelocity.x <= BreakagePoint && ColliderLinearVelocity.y <= BreakagePoint && ColliderLinearVelocity.z <= BreakagePoint)
+                    ColliderMovement = false;
+                if (ColliderAngularVelocity.x <= BreakagePoint && ColliderAngularVelocity.y <= BreakagePoint && ColliderAngularVelocity.z <= BreakagePoint)
+                    ColliderMovement = false;
+
+                if (ColliderMovement) goto BreakObject;
+                else return; // Returns if the breakable is not hit hard enough
+            }
+            else return; // Returns if the breakable is at rest and nothing hits it
+        }
+
+        Debug.Log($"{this.gameObject.name} collided with {collision.collider.name} (Velocity: {ObjectVelocity})");
+
+        // Check if breakble is dropped high or thrown hard enough
         if (ObjectVelocity.x <= BreakagePoint && ObjectVelocity.y <= BreakagePoint && ObjectVelocity.z <= BreakagePoint) return;
 
+        // Break apart breakbable
+        BreakObject:
         foreach (GameObject piece in ObjectPieces)
         {
             piece.GetComponent<Collider>().enabled = true;
@@ -57,6 +92,9 @@ public class ObjectShatter : MonoBehaviour
             rb.angularDamping = AngularDampening;
         }
 
+        Debug.Log($"<color=#00ff00>{this.gameObject.name} has broken.");
+
+        // Ensures that the object behaves as if it is truly broken
         Destroy(GetComponent<Collider>());
         Destroy(GetComponent<Rigidbody>());
     }
